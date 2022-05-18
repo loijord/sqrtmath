789b17cda04f5a93f986fd01e08764fc0607a8be
loijord <simonas.mamaitis@gmail.com>
2022-05-18 18:10:54 +0300

    replace initial web with graph structure

--- sidebar.py
+++ sidebar.py
@@ -1,38 +1,67 @@
-"""
-This app creates a simple sidebar layout using inline style arguments and the
-dbc.Nav component.
-
-dcc.Location is used to track the current location, and a callback uses the
-current location to render the appropriate page content. The active prop of
-each NavLink is set automatically according to the current pathname. To use
-this feature you must install dash-bootstrap-components >= 0.11.0.
-
-For more details on building multi-page Dash applications, check out the Dash
-documentation: https://dash.plot.ly/urls
-"""
 import dash
-from dash import Input, Output, dcc, html
+import dash_cytoscape as cyto
+from dash import html, dcc
+from dash.dependencies import Input, Output
+
+#demo for adding urls: https://stackoverflow.com/a/69700675/3044825
+cyto.load_extra_layouts() #dagre layout
+
+P1 = {'data': {'id': 'p1', 'label': 'Use Bulb'}, 'grabbable': True, 'classes': 'process'}
+P2 = {'data': {'id': 'p2', 'label': 'Prod. Bulb'}, 'grabbable': True, 'classes': 'process'}
+P3 = {'data': {'id': 'p3', 'label': 'Prod. Elec', 'parent': 'm1'}, 'grabbable': True, 'classes': 'process'}
+P4 = {'data': {'id': 'p4', 'label': 'Very long line for testing'}, 'grabbable': True, 'classes': 'process'}
+P5 = {'data': {'id': 'p5', 'label': 'Prod. Glass'}, 'grabbable': True, 'classes': 'process'}
+P6 = {'data': {'id': 'p6', 'label': 'Prod. Copper'}, 'grabbable': True, 'classes': 'process'}
+P7 = {'data': {'id': 'p7', 'label': 'Prod. Fuel', 'parent': 'm1'}, 'grabbable': True, 'classes': 'process'}
+
+E1 = {'data': {'id': 'e1', 'source': 'p7', 'target': 'p3', 'label': 'Fuel'}}
+E2 = {'data': {'id': 'e2', 'source': 'p3', 'target': 'p6', 'label': 'Elec.'}}
+E3 = {'data': {'id': 'e3', 'source': 'p3', 'target': 'p2', 'label': 'Elec.'}}
+E4 = {'data': {'id': 'e4', 'source': 'p3', 'target': 'p5', 'label': 'Elec.'}}
+E5 = {'data': {'id': 'e5', 'source': 'p3', 'target': 'p1', 'label': 'Elec.'}}
+E6 = {'data': {'id': 'e6', 'source': 'p6', 'target': 'p2', 'label': 'Copper'}}
+E7 = {'data': {'id': 'e7', 'source': 'p5', 'target': 'p2', 'label': 'Glass'}}
+E8 = {'data': {'id': 'e8', 'source': 'p2', 'target': 'p1', 'label': 'Bulb'}}
+E9 = {'data': {'id': 'e9', 'source': 'p4', 'target': 'p1', 'label': 'Waste Treatment'}}
+
+nodes = [P1, P2, P3, P4, P5, P6, P7]
+edges = [E1, E2, E3, E4, E5, E6, E7, E8, E9]
+
+stylesheet = [
+    # Group selectors
+    {'selector': 'node', 'style': {'content': 'data(label)', 'font-size': 8}},
+    {'selector': 'edge',
+     'style': {'content': 'data(label)',
+               'curve-style': 'unbundled-bezier',
+               'width': 1,
+               'line-color': 'lightblue',
+               'target-arrow-color': 'lightblue',
+               'target-arrow-shape': 'triangle',
+               'text-margin-x': 0,
+               'font-size': 8}},
+
+    # Class selectors
+    {'selector': '.process',
+     'style': {'shape': 'round-rectangle',
+               'background-color': 'white',
+               'border-color': 'black',
+               'border-width': 1,
+               'text-valign': 'center',
+               'height': 40,
+               'width': 75}}]
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.H6("Change the value in the text box to see callbacks in action!"),
-    html.Div([
-        "Input: ",
-        dcc.Input(id='my-input', value='initial value', type='text')
-    ]),
-    html.Br(),
-    html.Div(id='my-output'),
-
-])
-
-
-@app.callback(
-    Output(component_id='my-output', component_property='children'),
-    Input(component_id='my-input', component_property='value')
-)
-def update_output_div(input_value):
-    return f'Output: {input_value}'
+    dcc.Location(id="location"),
+    cyto.Cytoscape(
+        id='cytoscape',
+        layout={'name': 'dagre', 'spacingFactor': 1.15},
+        style={'width': '100%', 'height': '900px'},
+        stylesheet=stylesheet,
+        elements=nodes+edges,
+        autounselectify=True
+    )])
 
 server = app.server
-#if __name__ == "__main__":  app.run_server(port=8900)
+#if __name__ == "__main__":  app.run_server(port=8906)
