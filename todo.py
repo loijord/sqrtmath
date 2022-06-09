adbb5a247dfab3dac2f1bc731d338005e9898125
loijord <simonas.mamaitis@gmail.com>
2022-06-09 16:33:18 +0300

    asset upload

--- todo.py
+++ todo.py
@@ -14,3 +14,13 @@ data = {'tiesinė lygtis': '1.9.5.1', 'kryptinė tiesinė lygtis': '1.9.5.1'}
 # Sąvokos
 data = {'tiesinė lygtis': r'lygtis, kurios pavidalas yra $ax+by+c=0$, $a \neq 0$ ir $b\neq 0$',
         'kryptinė tiesinė lygtis': r'lygtis, kurios pavidalas yra $y=kx+m$'}
+
+# https://nbviewer.org/github/loijord/matematikos_pamokos/blob/master/programa/Mantas/stojamieji/pirmas_testas/darbo_planas.ipynb
+# https://github.com/loijord/matematikos_pamokos/blob/master/kraitis/s%C4%85vok%C5%B3_%C5%BEodyn%C4%97lis.ipynb
+# https://github.com/loijord/matematikos_pamokos/blob/master/kraitis/mindmaps/mindmaps.ipynb
+# https://callysto.github.io/shorts-book/shorts/Geogebra.html
+# https://dash.plotly.com/external-resources
+# https://wiki.geogebra.org/en/Reference:GeoGebra_Apps_Embedding
+# https://github.com/geogebra/math-apps-examples
+# https://help.geogebra.org/topic/jupyter-ipython-integration
+# https://compasstech.com.au/learn5/#lesson12
