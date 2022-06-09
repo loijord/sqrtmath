96520ac0773b07859159a04e2766af11dc1ffb8e
loijord <simonas.mamaitis@gmail.com>
2022-06-09 11:56:10 +0300

    asset upload

--- contents/part1.py
+++ contents/part1.py
@@ -28,31 +28,50 @@ html.Img(src=Image.open("assets/pitagoro_demo.png"), style={'width': '60%'}),
 dcc.Markdown(r"""Vėliau taip pat pagal šį modelį nubrėžiau lygybės 
 $(a^n)^m = a^{nm}$ įrodymo schemą:""", mathjax=True, style={'width': '60%'}),
 html.Img(src=Image.open("assets/laipsniu_demo.png"), style={'width': '60%'}),
-dcc.Markdown(r"""Dar vėliau paaiškėjo, kad šis modelis nėra toks lankstus, kaip atrodo. 
-Nors jis ir leidžia matyti duotus duomenis ir taikomas taisykles, tačiau dažnai yra sunku atskirti, 
-kaip reiktų užrašyti faktus ir taisyklę. Pvz. jei $a+b=2$ ir gavome, jog $a=1$, tai pagal tam tikrą taisyklę $b=1$. 
-Faktai tampa užrašomais sudėtingai: $\begin{cases} a+b=2 \\ a=1 \end{cases}$. 
-Jei tokių faktų susidaro daug, schema tampa perpildyta. Būna, kad dalis teiginių gali 
-priklausyti vienam faktui, o dalis kitam. Be to, minėtas faktas taptų suprantamas daug paprasčiau, jei sprendžiantysis 
-mąsto apie tam tikrą situaciją, pvz. apie šalį, kurioje yra du didmiesčiai ($a+b=2$), o prie jūros yra vienas iš jų ($a=1$).
+dcc.Markdown(r"""Dar vėliau paaiškėjo, kad šis modelis nėra toks lankstus, kaip atrodo. Taikant siūlomą būdą aprašyti sprendimus kiekviename uždavinyje kildavo daug klausimų dėl 
+teiginių ir taisyklių užrašymų. Keletas iškilusių sunkumų pavyzdžių: 
+* Jei duota, kad $a+b=2$ ir $a=1$, tai pagal tam tikrą taisyklę gausime $b=1$. Šiuo atveju norint aprašyti tai, kas duota,
+prireiktų naudoti komplikuotą teiginio užrašymą: $\begin{cases} a+b=2 \\ a=1 \end{cases}$. Jei taip aprašomų teiginių 
+sprendime pasitaiko daug, schema tampa perpildyta. 
+* Pasitaiko, kad dalis vieną teiginį sudarančių lygybių priklauso kitam kurioje nors sprendimo dalyje taikomam teiginiui. 
+Tokiu atveju būtų neišvengiama pasikartojamumo
+* Taisyklės taikymas pademonstruotam teiginiui gerokai supaprastėja, jei sprendžiantysis 
+mąsto apie tam tikrą kontekstą, pvz. apie šalį, kurioje yra du didmiesčiai, iš kurių dalis yra uostamiesčiai ($a+b=2$), 
+o jūra skalauja tik vieną iš jų ($a=1$). Tuomet lygčių arba lygčių sistemų sprendimas nebūtų reikalingas, užtektų teiginį
+matyti kita forma, kaip tai įprasta pradinukams. Kyla natūralus klausimas, 
+kurį iš šių sprendimo būdų verta rinktis aprašinėjant sprendimą, ir ar šiuos du sprendimo būdus laikyti skirtingais?
 
-2022 metais radau lankstesnį būdą pavaizduoti uždavinių sprendimui. 
+2021 m. kovo mėnesį viename iš 
+[Matematikos švietimo centro](http://mif.vu.lt/lt3/mokslas/projektai/163-lt/institutai/matematikos#centro-veiklos) 
+organizuojamų seminarų pristačiau savo tuometinę koncepciją apie mokyklinio matematikos turinio 
+sudarymą ir nuoseklų išdėstymą remiantis taikant panašias struktūras surinkta informacija apie dedukcinį ryšį ir 
+moksleivių pasiekimus. Skaidrės prieinamos adresu: 
+
+[https://github.com/loijord/matematikos_pamokos/blob/master/kraitis/teorija_to_praktika/skaidruoles.pdf]
+(https://github.com/loijord/matematikos_pamokos/blob/master/kraitis/teorija_to_praktika/skaidruoles.pdf)
+
+Skaidrėse įvardijau ir kitus iššūkius:
+
+* Uždavinių su keliais sprendimais aprašymas yra technologiškai kur kas sunkiau įgyvendinamas
+* Kai kurie samprotavimo principai yra sunkiai aprašomi. Pavyzdžiui įrodymas prieštaros būdu arba 
+*Modus Ponens* loginis principas
+ 
+2022 metų balandį radau lankstesnį būdą pavaizduoti uždavinių sprendimui. 
 Kaip pavyzdį nagrinėjau geometrijos uždavinį, kuriame reikia įrodyti, kad sujungus bet kurio keturkampio gretimų 
 kraštinių vidurio taškus gausime lygiagretainį:""", mathjax=True, style={'width': '60%'}),
 html.Img(src=Image.open("assets/netgraph_demo.jpg"), style={'width': '40%'}),
 dcc.Markdown("""Ankstesnėje schemoje rodyklės kartais būdavo nukreipiamos į kitas rodykles taip parodant, 
-kad norint pritaikyti tam tikrą sudėtingesnę taisyklę reikia žinoti tam tikrus nesudėtingus teiginius 
-ar paprastesnes taisykles. Šiame modelyje rodyklės gali eiti tik iš vieno teiginio į kitą. 
-Visos į tam tikrą teiginį einančios rodyklės nurodo būtinąsias sąlygas pritaikyti jam pagrįsti.
+kad norint pritaikyti tam tikrą sudėtingesnę taisyklę reikia taikyti kitus nesudėtingus teiginius 
+ar paprastesnes taisykles. Šiame modelyje susitarta, kad rodyklės gali eiti tik iš vieno teiginio į kitą. 
 Kiekvieno uždavinio sprendimą siūloma aprašinėti kaip kryptinį grafą, kurio viršūnių pavadinimai 
-yra tarpiniai teiginiai, o briaunų pavadinimai atitinka taikomų taisyklių pavadinimus.
+yra teiginiai, o briaunų, nukreiptų į tą pačią viršūnę, grupės atitinka, kažkurią vieną taisyklę.
 
-Taip pat 2022 metais pavyko atrasti būdą tokius grafus interaktyviai vaizduoti naudojant Python
-(*žr. App Gallery -> Interactive Graphs*). Grafai yra plačiai naudojama duomenų struktūra, 
+Praėjus mėnesiui pavyko atrasti ir techninį būdą tokius grafus interaktyviai vaizduoti (*žr. Home -> App Gallery -> Interactive Graphs*). Grafai yra plačiai naudojama duomenų struktūra, 
 žadanti daug galimybių, tame tarpe ir automatinį sprendimo generavimą naudojant kelio iš duotų duomenų į 
-ieškomus duomenis paiešką. Štai taip atrodo automatiškai iš pateikto grafo sugeneruotas sprendimas""",
+ieškomus duomenis paiešką. Norint taikyti tokius algoritmus būtina turėti tvarkingai aprašytus grafus. 
+Štai taip atrodo automatiškai iš pateikto grafo sugeneruotas sprendimas:""",
              mathjax=True, style={'width': '60%'}),
 html.Img(src=Image.open("assets/solution_demo.jpg"), style={'width': '60%'}),
-dcc.Markdown('''Kitoje dalyje nagrinėsim, kas turėtų įeiti į šią duomenų struktūrą, 
-kad ji būtų pakankama aprašyti mokyklinės matematikos mokomąją medžiagą ir kuo 
-tiksliau nustatyti moksleivių gebėjimus''', mathjax=True, style={'width': '60%'})])
+dcc.Markdown('''Kitoje dalyje nagrinėsime, kas turėtų įeiti į duomenų bazę, 
+pakankamą aprašyti mokyklinės matematikos mokomąją medžiagą ir teikti moksleiviams informaciją apie jų gebėjimus''',
+             mathjax=True, style={'width': '60%'})])
